523b6ac0345db41193634db58ee12622efbb554e
unknown <ardabitchy02@naver.com>
2020-08-12 00:12:07 +0900

    test

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 // ivew UI
-import ivenKO from 'iview/dist/locale/ko-KO'
 import ivenUS from 'iview/dist/locale/en-US'
 import ivzhCN from 'iview/dist/locale/zh-CN'
 import ivzhTW from 'iview/dist/locale/zh-TW'
@@ -14,7 +13,7 @@ import elzhTW from 'element-ui/lib/locale/lang/zh-TW'
 Vue.use(VueI18n)
 
 const languages = [
-  {value: 'ko-KO', label: '한국어', iv: ivenKO, el: elenKO},
+  {value: 'ko-KO', label: '한국어', iv: ivenUS, el: elenKO},
   {value: 'en-US', label: 'English', iv: ivenUS, el: elenUS},
   {value: 'zh-CN', label: '简体中文', iv: ivzhCN, el: elzhCN},
   {value: 'zh-TW', label: '繁體中文', iv: ivzhTW, el: elzhTW}
