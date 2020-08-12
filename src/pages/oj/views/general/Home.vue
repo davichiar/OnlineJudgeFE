edddd9a5768645d0f16cf1a657ca88297ffb9c4e
정다비치 <ardabitchy02@naver.com>
2020-08-12 09:48:13 +0900

    test

--- src/pages/oj/views/general/Home.vue
+++ src/pages/oj/views/general/Home.vue
@@ -4,8 +4,11 @@
     <div class="content markdown-body">
       <ul>
         <li>01. 로그인 가이드
-          <p>관리자 계정 로그인 방법 [아이디 : root 비밀번호 : rootroot]<br>
-          (관리자 계정 - 문제 업로드, 삭제 가능)
+          <p>관리자 계정 로그인 방법 [아이디 : root, 비밀번호 : rootroot]<br>
+          (관리자 계정 - 관련 공부한 문제 제출, 삭제 가능)<br><br>
+
+          게스트 계정 로그인 방법 [아이디 : guest, 비밀번호 : guest]<br>
+          (게스트 계정 - 문제 제출 가능)
           </p>
         </li>
       </ul>
